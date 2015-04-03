d3b0b858e25c3f434a9bc4906b9f38b84083dda7
unknown <kyle.w.r@hotmail.com>
2015-04-03 11:04:56 -0700

    added ability scores

--- index.php
+++ index.php
@@ -42,11 +42,8 @@
             </div>
 	</div>
 	<div class="container gray_l">
-            <?php $segment3 = array(array(array("Score", 3),array("Initiative", 4),array("DEX", 2),array("1/2 Lvl", 2),array("Misc", 1)),
-            array(array("Score", 3),array("AC", 4),array("ACMod", 1),array("Armor", 1),array("Class", 1), array('misc', 1),array('misc',1)), 
-            array(array('Score', 3),array('DEX',4),array('mod',3),array('misc',2)));
-
-            for($i=0; $i<count($segment3); $i++){
+            <?php
+            for($i=0; $i<3; $i++){
 		echo '<div class="col-sm-4">
                       <div class="row">
                       <div class="col-sm-12 title gray">';
@@ -58,16 +55,18 @@
                     echo '<h3> MOVEMENT </h3>';
 		}
 		echo '</div>
-                      </div>
-                      <div class="row">';
+                      </div>';            
         if($i == 0){
-			for ($j = 0; $j<4; $j++){
-	                    echo '<div class="col-sm-' . $segment3[$i][$j][1] . ' gray">
-	                                <label class="tiny">' . $segment3[$i][$j][0] . '</label>
+        	echo '<div class="row">';
+        	$segment3 = array(array("Score", 3),array("Initiative", 3),array("DEX", 2),array("1/2Lvl", 2),array("Misc", 2));
+           
+			for ($j = 0; $j<5; $j++){
+	                    echo '<div class="col-sm-' . $segment3[$j][1] . ' gray">
+	                                <label class="tiny">' . $segment3[$j][0] . '</label>
 	                                <h3 class="target"></h3>
-				  </div>';
+				</div>';
 			} 
-			
+			echo '</div>';
 			echo '<div class="row">
 	                        <div class="col-sm-8">
 	                            <label class="tiny"> Conditional Modifiers </label> 
@@ -77,53 +76,81 @@
 	                            <button class="btn btn-default space-fix"> Add </button> 
 				</div>
 	        </div>';
-			}
-			$segment4 = array(array("Score", 3,false),array("STRENGTH", 4,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false),
-				array("Score", 2,false),array("CONSTITUTION", 4,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false),
-				array("Score", 2,false),array("DEXTERITY", 4,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false),
-				array("Score", 2,false),array("INTELLIGENCE", 4,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false),
-				array("Score", 2,false),array("WISDOM", 4,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false),
-				array("Score", 2,false),array("CHARISMA", 4,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false));
-
-			echo '<div class="col-sm-12 title gray">
+			$segment4 = array(array(array("Score", 3,false),array("STRENGTH", 3,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false)),
+				array(array("Score", 3,false),array("CONSTITUTION", 3,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false)),
+				array(array("Score", 3,false),array("DEXTERITY", 3,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false)),
+				array(array("Score", 3,false),array("INTELLIGENCE", 3,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false)),
+				array(array("Score", 3,false),array("WISDOM", 3,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false)),
+				array(array("Score", 3,false),array("CHARISMA", 3,true),array("Abil  Mod", 3,false),array("1/2 Lvl", 3,false)));
+			echo '<div class="row"><div class="col-sm-12 title gray">
 				<h3> ABILITY SCORES </h3>
+					</div>
 				</div>
 				<div class="row">
-				<div class="col-sm-2">
-					<p class="tiny"> Score </p>
+					<div class="col-sm-3 gray">
+						<p class="tiny "> Score </p>
+					</div>
+					<div class="col-sm-3 gray">
+						<p class="tiny "> Ability </p>
+					</div>
+					<div class="col-sm-3 gray">
+						<p class="tiny "> Abil Mod </p>
+					</div>
+					<div class="col-sm-3 gray">
+						<p class="tiny "> Mod + 1/2 lvl </p>
+					</div>
+			</div>';
+			for ($m=0;$m<6;$m++){
+				echo '<div class="row">';
+				for ($l=0;$l<4; $l++){
+					if($l == 0 || $l == 1){
+						if($segment4[$m][$l][2]){
+
+							echo '<div class="col-sm-' . $segment4[$m][$l][1] . '">';
+								echo '<h4 class="tiny">' . $segment4[$m][$l][0] . '</h4>
+								</div>';
+						}else{				
+							echo '<div class="col-sm-' . $segment4[$m][$l][1] . '">';
+							echo '<input type="number" class="form-control">
+						</div>';
+						}
+					}else{
+						echo '<div class="col-sm-' . $segment4[$m][$l][1] . ' gray">';
+						echo   '<h3 class="target"></h3>
+						</div>';
+					}
+				}
+				echo '</div>';
+			}
+			}else if($i == 1){
+			 	for ($n = 0; $n<4; $n++){
+			 		$segment5 = array(array(array("Score", 2),array("AC", 3),array("armor", 1),array("lmod", 1),array("class", 1),array("feat", 1),array("ENH", 1),array("misc", 1)),
+			 						array(array("Score", 2),array("FORT", 3),array("armor", 1),array("lmod", 1),array("class", 1),array("feat", 1),array("ENH", 1),array("misc", 1)),
+			 						array(array("Score", 2),array("REF", 3),array("armor", 1),array("lmod", 1),array("class", 1),array("feat", 1),array("ENH", 1),array("misc", 1)),
+			 						array(array("Score", 2),array("WILL", 3),array("armor", 1),array("lmod", 1),array("class", 1),array("feat", 1),array("ENH", 1),array("misc", 1)));
+			 			echo '<div class="row">';
+			 			for($o=0;$o<8;$o++){
+	                    echo '<div class="col-sm-' . $segment5[$n][$o][1] . ' gray">
+	                                <label class="tiny">' . $segment5[$n][$o][0] . '</label>
+	                                <h3 class="target"></h3>
+				  				</div>';
+			}
+				echo  '</div>';
+				
+			echo '<div class="row">
+	                        <div class="col-sm-8">
+	                            <label class="tiny"> Conditional Modifiers </label> 
+					<input class="form-control">
 				</div>
 				<div class="col-sm-4">
-					<p class="tiny"> Ability </p>
+	                            <button class="btn btn-default space-fix"> Add </button> 
 				</div>
-				<div class="col-sm-3">
-					<p class="tiny"> Abil Mod </p>
-				</div>
-				<div class="col-sm-3">
-					<p class="tiny"> Mod + 1/2 lvl </p>
-				</div>
-			</div>';
-
-			for ($l=0;$l<24; $l++){
-				if($segment4[$l][2]){
-
-					echo '<div class="col-sm-' . $segment4[$l][1] . '">';
-						echo '<h4>' . $segment[$l][0] . '</h4>
-						</div>';
-				}else{
-					if(($segment4[$l]-20)%3 == 0 || ($segment4[$l]-20)%4 == 0){
-					echo '<div class="col-sm-' . $segment4[$l][1] . '">';
-					echo '<input class="form-control">
-				</div>';
-				}else{
-					echo '<div class="col-sm-' . $segment4[$l][1] . '">';
-					echo '<input type="number" class="form-control">
-				</div>
-				</div>';
-
-				}
+	        </div>';
+	    		}
 			}
-		}
-	}?>
+			echo '</div>';
+		}?>
+			</div>
 		</div>	
     </body>
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
