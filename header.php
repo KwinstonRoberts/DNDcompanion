7898b87241971854cbafb9ea4d92601f45aa4a04
EquinoxEarth <colefinnie@hotmail.com>
2015-04-06 07:38:56 -0400

    Formatting change

---
+++ header.php
@@ -0,0 +1,59 @@
+<header>
+    <div class="container gray">
+        <div class="row">
+            <div class="col-sm-8 light">
+                <h1> DND Campaign Companion </h1>
+            </div>
+            <div class="col-sm-4 light">
+                <label for="playername">Player's Name</label> 
+                <input class="form-control" type="text" id="playername">
+            </div>
+        </div>
+    </div>
+    <div class="container gray_l">
+        <div class="row">
+            <div class="col-sm-2">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Players <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="btn-group">
+		    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Items <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                    </ul>
+		</div>
+            </div>
+            <div class="col-sm-2">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Locations <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                    </ul>
+		</div>
+            </div>
+            <div class="col-sm-2">
+		<div class="btn-group">
+		    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Monsters <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                    </ul>
+		</div>
+            </div>	
+            <div class="col-sm-2">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Archive <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> Modules <span class="caret"></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
