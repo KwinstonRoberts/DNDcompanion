258fc3d1482ec8afd6b44ac6bb60bb4bb5925fe5
kyle roberts <kyle.w.r@hotmail.com>
2015-04-07 10:58:10 -0400

    added error check

--- query.php
+++ query.php
@@ -5,6 +5,10 @@
     $dbhost = "heroku_e5f1f5a101fb1b4";
     
     $conn = mysqli_connect($host, $user, $pass, $dbhost);
+    if(!$conn){
+        die("Connection Failed" . mysqli_connect_error())
+    }
+
     $name = $_POST[0];
     $value = $_POST[1];
     
