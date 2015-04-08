4db109a23305c774a9adb74ef710c790930557a9
EquinoxEarth <colefinnie@hotmail.com>
2015-04-08 19:39:52 -0400

    Variables ahoy, this needs to be organized badly.
    I wonder if we can use structs in javascript.

--- js/variables.js
+++ js/variables.js
@@ -9,26 +9,82 @@ var cEXP;       // Character experience
 var cParagon;   // Paragon Path
 var cDestiny;   // Epic Destiny
 
-var cSTR;       // Strength
-var cSTRam;     // Ability Mod
-var cSTRlvl;    // Mod + 1/2 Level
+// ABILITY SCORES //
+{
+    var STR;       // Strength
+    var STRam;     // Ability Mod
+    var STRlvl;    // Mod + 1/2 Level
+    
+    var CON;       // Constitution
+    var CONam;     // Ability Mod
+    var CONlvl;    // Mod + 1/2 Level
+    
+    var DEX;       // Dexterity
+    var DEXam;     // Ability Mod
+    var DEXlvl;    // Mod + 1/2 Level
+    
+    var INT;       // Intelligence
+    var INTam;     // Ability Mod
+    var INTlvl;    // Mod + 1/2 Level
+    
+    var WIS;       // Wisdom
+    var WISam;     // Ability Mod
+    var WISlvl;    // Mod + 1/2 Level
+    
+    var CHA;       // Charisma
+    var CHAam;     // Ability Mod
+    var CHAlvl;    // Mod + 1/2 Level
+}
 
-var cCON;       // Constitution
-var cCONam;     // Ability Mod
-var cCONlvl;    // Mod + 1/2 Level
+// DEFENSES //
+{
+    // AC //
+    var AC;
+    var ACarmor;
+    var AClmod;
+    var ACclass;
+    var ACfeat;
+    var ACenh;
+    var ACmisc;
+    var ACmisc2;
+    
+    // FORT //
+    var FORT;
+    var FORTarmor;
+    var FORTlmod;
+    var FORTclass;
+    var FORTfeat;
+    var FORTenh;
+    var FORTmisc;
+    var FORTmisc2;
+    
+    // REF //
+    var REF;
+    var REFarmor;
+    var REFlmod;
+    var REFclass;
+    var REFfeat;
+    var REFenh;
+    var REFmisc;
+    var REFmisc2;
+    
+    // WILL //
+    var WILL;
+    var WILLarmor;
+    var WILLlmod;
+    var WILLclass;
+    var WILLfeat;
+    var WILLenh;
+    var WILLmisc;
+    var WILLmisc2;
+}
 
-var cDEX;       // Dexterity
-var cDEXam;     // Ability Mod
-var cDEXlvl;    // Mod + 1/2 Level
-
-var cINT;       // Intelligence
-var cINTam;     // Ability Mod
-var cINTlvl;    // Mod + 1/2 Level
-
-var cWIS;       // Wisdom
-var cWISam;     // Ability Mod
-var cWISlvl;    // Mod + 1/2 Level
-
-var cCHA;       // Charisma
-var cCHAam;     // Ability Mod
-var cCHAlvl;    // Mod + 1/2 Level
+// HIT POINTS //
+{
+    var maxHP;    
+    var surgeValue;
+    var surgesPDay;
+    
+    var curHP;
+    var curSurges;
+}
