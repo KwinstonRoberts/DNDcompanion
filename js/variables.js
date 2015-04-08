7b01401a3bc58f5deeaf240ae49451c4031715fd
EquinoxEarth <colefinnie@hotmail.com>
2015-04-08 19:23:19 -0400

    Make way for dynamic sheet updating
    I added a ton of variables, yaay.

---
+++ js/variables.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var cClass;     // Character Class
+var cRace;      // Character Race
+var cEXP;       // Character experience
+
+var cParagon;   // Paragon Path
+var cDestiny;   // Epic Destiny
+
+var cSTR;       // Strength
+var cSTRam;     // Ability Mod
+var cSTRlvl;    // Mod + 1/2 Level
+
+var cCON;       // Constitution
+var cCONam;     // Ability Mod
+var cCONlvl;    // Mod + 1/2 Level
+
+var cDEX;       // Dexterity
+var cDEXam;     // Ability Mod
+var cDEXlvl;    // Mod + 1/2 Level
+
+var cINT;       // Intelligence
+var cINTam;     // Ability Mod
+var cINTlvl;    // Mod + 1/2 Level
+
+var cWIS;       // Wisdom
+var cWISam;     // Ability Mod
+var cWISlvl;    // Mod + 1/2 Level
+
+var cCHA;       // Charisma
+var cCHAam;     // Ability Mod
+var cCHAlvl;    // Mod + 1/2 Level
