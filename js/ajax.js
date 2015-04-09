9f222ab01e02ee5d1fd3180b407ace7704ad8dbc
unknown <kyle.w.r@hotmail.com>
2015-04-08 18:24:50 -0700

    some syntax fixes

--- js/ajax.js
+++ js/ajax.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
             for (var i=0; i<names.length; i++){
                 console.log(names[i]);
                 var html = $('#players').html();
-                $('#players').html(html + '<li><a>' + names[i] + '</a></li>');
+                $('#players').html(html + '<li class="player"><a>' + names[i] + '</a></li>');
             }
         }
     });
 });
 
-$('a').mousedown(function(event){
+$('li').click(function(event){
     event.preventDefault();
     console.log("!!!");
     $.ajax({
