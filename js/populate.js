88bb036c2acb6d5dff01338c3fc63a1c0c7b65dd
unknown <kyle.w.r@hotmail.com>
2015-04-09 19:06:03 -0700

    added file

---
+++ js/populate.js
@@ -0,0 +1,17 @@
+function collect(){
+	var character = $('[name="Character_Name"]').val();
+	var level = $('[name="Character_Level"]').val();
+	Class['name'] = $('[name="Class"]').val();
+	Paragon = $('[name="Character_Path"]').val();
+	Destiny = $('[name="Epic_Destiny"]').val();
+	EXP = $('[name="Exp"]').val();
+	Race['name'] = $('[name="Race"]').val();
+	var size = $('[name="Size"]').val();
+	var age = $('[name="Age"]').val();
+	var gender = $('[name="Gender"]').val();
+	var height = $('[name="Height"]').val();
+	var weight = $('[name="Weight"]').val();
+	var alignment = $('[name="Alignment"]').val();
+	var diety = $('[name="Diety"]').val();
+	var company = $('[name="Adventuring_company"]').val();
+}
