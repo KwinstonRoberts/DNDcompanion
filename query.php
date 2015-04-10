b88a2ed3bbede6dcf4f152d67f84460fada9e0a8
unknown <kyle.w.r@hotmail.com>
2015-04-09 17:28:42 -0700

    added an error check for testing

--- query.php
+++ query.php
@@ -60,12 +60,12 @@ if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
         	}
        	while($row = mysqli_fetch_row($result)){
        		$response = "";
-         	for ($i=0; $i<count($row); $i++){
-       	 		$columns = array('Character_Name','Character_Level','Class','Paragon_Path','Epic_Destiny',
+         	for ($i=1; $i<count($row); $i++){
+       	 		$columns = array("",'Character_Name','Character_Level','Class','Paragon_Path','Epic_Destiny',
                       'Exp','Race','Size','Age','Gender','Height',
                       'Weight','Alignment','Diety','Adventuring_Company');
          		
-     			mysqli_query($conn,"UPDATE players set $columns[$i+1]='$row[$i+1]' WHERE Player_Name = '$name'");
+     			mysqli_query($conn,"UPDATE players set $columns[$i] ='$row[$i]' WHERE Player_Name = '$name'");
                 $response = $response . $row[i];
         	}
         }
