748212a3d8b373b577c7aaadfd523d9398a1403f
unknown <kyle.w.r@hotmail.com>
2015-04-10 18:00:18 -0700

    made character level calculated

--- index.php
+++ index.php
@@ -241,16 +241,16 @@
 								</div>';
 					}
 				}
+				echo '</div>';
 			}
-				         echo   '<div class="col-md-8">
+				         echo   '<div class="row"><div class="col-md-8">
 		                            <label class="tiny"> Special Senses </label> 
 						<input name="special-movement" class="form-control">
 					</div>
 					<div class="col-md-4">
 		                            <button class="btn btn-default space-fix"> Add </button> 
-					</div>
-		        	      </div>';
-				echo '</div>';
+					</div>';
+			echo '</div>';
 					echo   '<div class="row">
 					   <div class="col-md-12 title gray">
 							<h3> FEATS </h3>
@@ -264,8 +264,8 @@
 		                    <button class="btn btn-default space-fix"> Add </button> 
 						</div>
 					</div>';
+			}
 			echo '</div>';
-		}
 	}?>
 		</div>
 	</div>	
