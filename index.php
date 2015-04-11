4b2dc93a7ce46491a295cd1c536a03c32836760b
unknown <kyle.w.r@hotmail.com>
2015-04-11 15:55:01 -0700

    fixed senses

--- index.php
+++ index.php
@@ -161,8 +161,12 @@
 								<h3 class="tiny"> Second Wind 1/Encounter </h3>
 							</div>';
 							echo '<div class="row">
-							<div class="col-md-4">
-								<span><h3 class="tiny"> USED </h3> <input class="form-control" type="checkbox" name="2ndWind"></span>
+							<div class="col-md-2">
+							 <h3 class="tiny"> USED </h3>
+							</div>';
+							echo '<div class="row">
+							<div class="col-md-2">
+							 <input class="form-control" type="checkbox" name="2ndWind">
 							</div></div>';
 							echo '<div class="row">
 							<div class="col-md-12">
