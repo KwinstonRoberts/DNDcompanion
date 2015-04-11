03b380f5192f5dc2b81d50acc692a1f865ef2fd4
unknown <kyle.w.r@hotmail.com>
2015-04-10 18:06:58 -0700

    added senses

--- index.php
+++ index.php
@@ -204,8 +204,7 @@
 					</div>
 					<div class="col-md-4">
 		                            <button class="btn btn-default space-fix"> Add </button> 
-					</div>
-		        	      </div>';
+					</div>';
 		echo '</div>';
 		echo '<div class="row">
 				<div class="col-md-12 title gray">
