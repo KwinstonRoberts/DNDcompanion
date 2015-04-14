95cc7c68637ad8c24b78f5c4733d79e3dd1cad55
kyle roberts <kyle.w.r@hotmail.com>
2015-04-14 11:38:07 -0400

    added calculations

--- js/ajax.js
+++ js/ajax.js
@@ -97,7 +97,7 @@ $(document).ready(function(){
                 header: 2},
             success: function(response){
                 console.log(response);
-                calculate();
+                calculator();
             }
         });
     });
