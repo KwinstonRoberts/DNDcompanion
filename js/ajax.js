ef6ae11a3b4edec723768d1bf148a93ca704f3f6
kyle roberts <kyle.w.r@hotmail.com>
2015-04-14 14:49:05 -0400

    added calculations

--- js/ajax.js
+++ js/ajax.js
@@ -109,7 +109,7 @@ $(document).ready(function(){
                 $('#target-hp').text(names[0]);
                 calculator();
                 }           
-            }
+            });
         });
     });
 });
