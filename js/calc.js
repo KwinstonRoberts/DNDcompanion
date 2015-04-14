5b922e54c1b06fb17814a0e192e5e7986d621cc7
kyle roberts <kyle.w.r@hotmail.com>
2015-04-14 12:25:37 -0400

    added calculations

--- js/calc.js
+++ js/calc.js
@@ -2,8 +2,8 @@ function calculator(){
 	var exp_milestones = [1000,2250,3750,5500,7500,10000,13000,16500,20500,26000,32000,39000,47000,57000,69000,83000,99000,119000,143000,175000,210000,255000,310000,375000,45000,550000,675000,825000,1000000];
 	for(var i=0; i<exp_milestones.length; i++){
 		if(Char['experience']<exp_milestones[i] && Char['experience']>exp_milestones[i-1]){
-			Char['level']=i;
-			$('[name="Character_level"]').text=="" + (i);
+			Char['level']=i+1;
+			$('[name="Character_level"]').text=="" + (i+1);
 			break;
 		}
 	}
