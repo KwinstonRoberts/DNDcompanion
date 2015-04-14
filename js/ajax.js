b948f43198c4c23e92a4a5c855a4abf243602e66
kyle roberts <kyle.w.r@hotmail.com>
2015-04-14 15:37:35 -0400

    added calculations

--- js/ajax.js
+++ js/ajax.js
@@ -97,7 +97,6 @@ $(document).ready(function(){
                     header: 2},
             success: function(response){
                 console.log(response);
-
                 $.ajax({
                     type: 'POST',
                     url: 'query.php',
@@ -105,7 +104,7 @@ $(document).ready(function(){
                     Class : Char['cclass'], 
                     header: 3},
                     success: function(response){
-                        names = response.split(',');
+                        var names = response.split(',');
                         $('#target-hp').text(names[0]);
                         calculator();
                     } 
