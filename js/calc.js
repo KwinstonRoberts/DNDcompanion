70e7e17d880bf6282e23e8eed2aa9735075048fc
tyakudiscora <tyler_hi@live.ca>
2015-09-07 21:53:53 -0400

    fixing
    stuff

--- js/calc.js
+++ js/calc.js
@@ -23,7 +23,7 @@ function calculator(){
 	}
 	//this is the start of the hp calculations
 	var lvlhp = 0;
-	
+	$('.target-HP').text(22);
 	if($('[name="Class"]').value == "Fighter"){
 		if(Char['level'] > 1){
            for(var x=0; x < Char['level'] - 1; x++){
